c69aa2b8b05478587d1cb7003e33f34d3a6b039f
jordiGarcia <jordigarcia81@hotmail.com>
2020-09-24 20:42:56 +0100

    subimos ejercicio 2

--- AE1/src/es/florida/App.java
+++ AE1/src/es/florida/App.java
@@ -1,7 +1,32 @@
 package es.florida;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void sayHello() {
         System.out.println("hola mundo");
     }
+
+    public static void arrayNombres(){
+    String[]array={"jordi","jesus","damia","juan","pablo","rafa"};
+    List<String> array2 = new ArrayList<>();
+        array2.add("jordi");
+        array2.add("jesus");
+        array2.add("damia");
+        array2.add("juan");
+        array2.add("pablo");
+        array2.add("rafa");
+
+        for (int i=0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+
+        for(String i: array2){
+            System.out.println(i);
+        }
+
+
+    }
 }
+
--- AE1/src/es/florida/Main.java
+++ AE1/src/es/florida/Main.java
@@ -3,6 +3,9 @@ package es.florida;
 public class Main {
 
     public static void main(String[] args) {
-	App.sayHello();
+        App.sayHello();
+        App.arrayNombres();
     }
 }
+
+
